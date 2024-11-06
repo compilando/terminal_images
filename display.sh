7779d2858823675f69283b811aeea3ed4d7a815f
compilando <oscargonzalezmoreno@gmail.com>
2024-11-06 12:27:03 +0100

    README.md

--- display.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Verificar si el archivo ASCII existe
-if [ -f "output_colorscript.txt" ]; then
-    cat output_colorscript.txt
-else
-    echo "El archivo output_colorscript.txt no existe. Asegúrate de ejecutar el script de Python primero."
-fi
